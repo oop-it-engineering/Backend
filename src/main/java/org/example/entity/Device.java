d247bcc470e2b762f2188fef265ca4df1c6ed7b0
dylee00 <dylee@sookmyung.ac.kr>
2024-01-07 20:10:11 +0900

    생성자, getter&setter 추가

--- src/main/java/org/example/entity/Device.java
+++ src/main/java/org/example/entity/Device.java
@@ -10,6 +10,65 @@ public class Device {
     private boolean dResConf;   //장비 예약 확정 유무, 1이면 예약 확정 -> 대여중
     private boolean dStatus;    //장비 손상 및 분실 유무, 1이면 이용 가능한 장비
 
-    
+    public Device() {}  //생성자
+
+    //getter&setter
+
+
+    public int getDeviceId() {
+        return deviceId;
+    }
+
+    public void setDeviceId(int deviceId) {
+        this.deviceId = deviceId;
+    }
+
+    public String getdName() {
+        return dName;
+    }
+
+    public void setdName(String dName) {
+        this.dName = dName;
+    }
+
+    public String getdCategory() {
+        return dCategory;
+    }
+
+    public void setdCategory(String dCategory) {
+        this.dCategory = dCategory;
+    }
+
+    public String getdVersion() {
+        return dVersion;
+    }
+
+    public void setdVersion(String dVersion) {
+        this.dVersion = dVersion;
+    }
+
+    public boolean isdRes() {
+        return dRes;
+    }
+
+    public void setdRes(boolean dRes) {
+        this.dRes = dRes;
+    }
+
+    public boolean isdResConf() {
+        return dResConf;
+    }
+
+    public void setdResConf(boolean dResConf) {
+        this.dResConf = dResConf;
+    }
+
+    public boolean isdStatus() {
+        return dStatus;
+    }
+
+    public void setdStatus(boolean dStatus) {
+        this.dStatus = dStatus;
+    }
 }
 
